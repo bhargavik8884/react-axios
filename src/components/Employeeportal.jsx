812db8d4f8d32f6db02fa5702f1cd8adfac5f81a
bhargavi <bhargavibhoomika@gmail.com>
2021-07-10 22:29:01 +0530

    done

---
+++ src/components/Employeeportal.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import "../App.css"
+export default function Employeeportal() {
+    return (
+        <div className="div-portal">
+            <div className="port-main">
+                <div>
+                <img src="https://blogimage.vantagecircle.com/vcblogimages/2020/08/Employee-recognition.png" alt="img" />
+                </div>
+                <p>
+                A great datagrid starts with a fundamentally sound grid. FlexGrid includes only the essential vital features and offers everything else through an extensibility model.
+                 Basic Excel-like features like sorting, grouping, and editing are built-in, while the bells and whistles are optional.
+
+    
+                </p>
+            </div>
+            <div className="port-main">
+
+                <p>
+                Wijmo JavaScript UI components will never bloat your web apps! Built for top performance, 
+                 components aren't just dependency-free, they are also built to be small, extensible, and fast.
+                
+                </p>
+                <div>
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSvKQurHJ1UVLQIgdx1t-AtBgLS8J1822Q34Q&usqp=CAU" alt="img" />
+                </div>
+            </div>
+        </div>
+    )
+}
--- src/router.js
+++ src/router.js
@@ -1,20 +1,13 @@
-
-import React,{useState} from "react";
-import { Link, BrowserRouter, Route, Switch, NavLink } from "react-router-dom";
+import {  BrowserRouter, Route, Switch, NavLink } from "react-router-dom";
 import Employeeportal from "./components/Employeeportal";
 import Employeelist from "./components/Employeelist";
 import Employeedetails from "./components/Employeedetail";
 import Logout from "./components/Logout";
 import Login from "./components/Login";
-function App() {
-  const [str, setStr] = useState("")
 
-  const check=(data)=>{
-    setStr(data)
-  }
- if(str.length !== 0){
-  return (
-    <BrowserRouter>
+
+export const routing = (
+  <BrowserRouter>
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
       <button
         className="navbar-toggler"
@@ -27,6 +20,7 @@ function App() {
       >
         <span className="navbar-toggler-icon"></span>
       </button>
+
       <div className="collapse navbar-collapse" id="navbarSupportedContent">
         <ul className="navbar-nav mr-auto">
           <li className="nav-item active">
@@ -35,9 +29,9 @@ function App() {
             </NavLink>
           </li>
           <li className="nav-item">
-            <Link className="nav-link" to="/home" >
+            <NavLink className="nav-link" to="/home" >
               Home
-            </Link>
+            </NavLink>
           </li>
 
           <li className="nav-item">
@@ -55,7 +49,11 @@ function App() {
               logout
             </NavLink>
           </li>
-
+          <li className="nav-item">
+            <NavLink className="nav-link " to="/login" activeClassName="btn btn-primary">
+              login
+            </NavLink>
+          </li>
         </ul>
       </div>
     </nav>
@@ -66,62 +64,7 @@ function App() {
     <Route path="/list" component={Employeelist}  />
     <Route path="/details" component={Employeedetails}  />
     <Route path="/logout" component={Logout}  />
-    <Route path="/login">
-      <Login data={str} send={check}/>
-   </Route>
-   </Switch>
+    <Route path="/login" component={Login}  />
+    </Switch>
   </BrowserRouter>
-
-  );
- }
- else{
-    return (
-      <BrowserRouter>
-      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav mr-auto">
-            <li className="nav-item active">
-              <NavLink className="nav-link" to="/home" activeClassName="btn btn-primary">
-                 Employee Portal
-              </NavLink>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/home" >
-                Home
-              </Link>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link " to="/login" activeClassName="btn btn-primary">
-                login
-              </NavLink>
-            </li>
-          </ul>
-        </div>
-      </nav>
-
-      <Switch>
-
-      <Route path="/home" component={Employeeportal } />
-      <Route path="/login">
-        <Login data={str} send={check}/>
-     </Route>
-     </Switch>
-    </BrowserRouter>
-
-    );
-
- }
-}
-
-export default App;
+);
